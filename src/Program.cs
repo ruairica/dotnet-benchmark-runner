cdac22ee6d54d67b43d67f5621eabc871869c0e4
Ruairi <60053464+ruairica@users.noreply.github.com>
2023-10-20 13:39:25 +0100

    project and main file

---
+++ src/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+public class ExistsVsAnyListComparison
+{
+    public List<int> NumbersList = Enumerable.Range(0, 10_000).ToList();
+
+    [Benchmark]
+    public void Any()
+    {
+        this.NumbersList.Any(x => x == 9_555);
+    }
+
+    [Benchmark]
+    public void Exists()
+    {
+        this.NumbersList.Exists(x => x == 9_555);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ExistsVsAnyListComparison>();
+    }
+}
